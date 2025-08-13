93afa9b4775743903983bb423b88152460659486
OmarAbdelAziz123 <3omarabdelaziz123@gmail.com>
2025-08-13 09:22:16 +0300

    Make change baseurl

--- lib/service/network/api/api.dart
+++ lib/service/network/api/api.dart
@@ -2,7 +2,8 @@
 import 'dart:io';
 
 // ignore: constant_identifier_names
-const String BASE_URL = "https://ma3rfa.backend.tanfeethi.tanfeethi.com.sa";
+// const String BASE_URL = "https://ma3rfa.backend.tanfeethi.tanfeethi.com.sa";
+const String BASE_URL = "https://newma3refabackend.tanfeethi.tanfeethi.com.sa";
 // "https://myacademy.moltaqadev.com";
 // "https://moazez-alta3lemy.com";
 // "https://myacademy.moltaqa-wo.net";
@@ -11,6 +12,7 @@ const appID = '27f16866203b45f59cbc5b53b0f13b08';
 
 // "https://myacademy.hishamelgez.com";
 // ignore: constant_identifier_names
+// const String ApiUrl = "http://192.168.1.15:8000/api";
 const String ApiUrl = "$BASE_URL/api";
 
 String key = Platform.isAndroid
